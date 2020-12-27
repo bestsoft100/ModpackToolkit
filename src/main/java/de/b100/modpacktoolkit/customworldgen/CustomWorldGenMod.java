b24a5a19ef729150535c794382d3fae92e7f92f6
bestsoft100 <49400900+bestsoft100@users.noreply.github.com>
2020-12-27 04:18:45 +0100

    Oops

--- src/main/java/de/b100/modpacktoolkit/customworldgen/CustomWorldGenMod.java
+++ src/main/java/de/b100/modpacktoolkit/customworldgen/CustomWorldGenMod.java
@@ -1,13 +1,16 @@
 package de.b100.modpacktoolkit.customworldgen;
 
 import de.b100.modpacktoolkit.ModpackToolkitMod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CustomWorldGenMod extends ModpackToolkitMod{
 	
 	public void init() {
+		WorldGenerator.instance = new WorldGenerator();
+		GameRegistry.registerWorldGenerator(WorldGenerator.instance, 1);
+		
 		WorldGenerator.biomeGroups = BiomeGroup.loadBiomeGroups(getModConfigFolder());
 		WorldGenerator.generators = Generator.loadGenerators(getModConfigFolder());
-		
 	}
 	
 	public String getName() {
